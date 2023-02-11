6b6bb26f0a9506acd6d95614d573c667791583dc
Martyna Zukowska <martynazukowska7@interia.pl>
2023-02-11 17:40:49 +0100

    listy, slownik, bool, variables

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
 print("Martyna")
 print(int("5")+3)
 
+
 print("Moje opcje to: "+ "A:B:C".split(":")[0])
+
+#słownik, nie lista odwolujemy się do kategorii a nie do 0/1/2
+slownik={"name": "Martyna", "age": 23, "hobby": "jedzenie"}
+
+print(slownik["name"])
