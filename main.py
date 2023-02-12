e4f91e141442bc4bc9819dc592d48ddbe880ee99
Martyna Zukowska <martynazukowska7@interia.pl>
2023-02-13 00:05:35 +0100

    funkcje

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+
+
 print("Martyna")
 print(int("5")+3)
 
@@ -8,3 +10,13 @@ print("Moje opcje to: "+ "A:B:C".split(":")[0])
 slownik={"name": "Martyna", "age": 23, "hobby": "jedzenie"}
 
 print(slownik["name"])
+print(len(slownik["name"]))
+
+kolejka=[3,55,67,22,34,5,21,4,67,99]
+
+print(sorted(kolejka))
+
+def moja_funkcja():
+    print("Funkcja")
+
+moja_funkcja()
