9a05bb8fac4f136477309154654c5c1f806b7a7c
Martyna Zukowska <martynazukowska7@interia.pl>
2023-02-13 21:16:10 +0100

    Wpisuj i rozwiązuj równania

--- main.py
+++ main.py
@@ -1,14 +1,28 @@
 import re
 
-print("Kalkulator")
+print("Kalkulator\n")
+
+# aby zakończyć rozgrywke należy wcisnąć q
 
 wczesniejszy=0
 
 run=True
 
 def wyswietl():
+    global run
+    global wczesniejszy
+
     rownanie=input("Wpisz dzialanie: ")
-    print(rownanie)
+    if rownanie=="q":
+        run=False
+    else:
+        # eliminacja wszystkich innych niewskazanych znaków w kalkulatorze
+        rownanie=re.sub("[A-Za-z,.:]","",rownanie)
+        if rownanie=="":
+            rownanie="0"
+        # eval() rozwiązuje podane rówanie 
+        wczesniejszy=eval(rownanie)
+        print(wczesniejszy)
 
 while run:
     wyswietl()
