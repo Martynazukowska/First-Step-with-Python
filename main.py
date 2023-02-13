598c2636f3f939083adfb9e2dfd4687aa398522c
Martyna Zukowska <martynazukowska7@interia.pl>
2023-02-13 21:30:18 +0100

    Prosty kalkulator skończony

--- main.py
+++ main.py
@@ -5,14 +5,24 @@ print("Kalkulator\n")
 # aby zakończyć rozgrywke należy wcisnąć q
 
 wczesniejszy=0
+start=0
+prewczesniejszy=0
 
 run=True
 
 def wyswietl():
     global run
     global wczesniejszy
+    global start
+    global prewczesniejszy
 
-    rownanie=input("Wpisz dzialanie: ")
+    #jeśli zaczynamy działanie
+    if start==0:
+        rownanie=input("Wpisz dzialanie: ")
+    #jeśli rozpisujemy wcześniejsze działanie
+    else:
+        rownanie=input(str(wczesniejszy))
+
     if rownanie=="q":
         run=False
     else:
@@ -20,9 +30,17 @@ def wyswietl():
         rownanie=re.sub("[A-Za-z,.:]","",rownanie)
         if rownanie=="":
             rownanie="0"
-        # eval() rozwiązuje podane rówanie 
-        wczesniejszy=eval(rownanie)
-        print(wczesniejszy)
+
+        if start==0:
+            prewczesniejszy=""
+            # eval() rozwiązuje podane rówanie 
+            wczesniejszy=eval(rownanie)
+            start=start+1
+        else:
+            prewczesniejszy=wczesniejszy
+            wczesniejszy=eval(str(wczesniejszy)+rownanie)
+
+        print(prewczesniejszy,rownanie,"=",wczesniejszy)
 
 while run:
     wyswietl()
