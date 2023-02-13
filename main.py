853a9682a9bdbecc67fc63687e2452519a4ec5df
Martyna Zukowska <martynazukowska7@interia.pl>
2023-02-13 01:14:51 +0100

    petle i reszta

--- main.py
+++ main.py
@@ -19,9 +19,30 @@ def moja_funkcja(pierwszy,drugi):
 moja_funkcja("Funkcja", -1)
 moja_funkcja("Teraz funkcja", 0)
 
-#tworzenie default wartości funkcji
-def moja_funkcja2(pierwszy="Uwaga błąd",drugi=-10001):
-    print(pierwszy,2+drugi)
+zmienna=[1,2,3,4]
 
-moja_funkcja2("Funkcja", -1)
-moja_funkcja2()
+#wszystkie wartości wchodzące do funkcji
+def spis_ludzi(*czlowiek):
+    for ktos in czlowiek:
+        print("Ten ktos to: ", ktos)
+
+for item in zmienna:
+    if item==1:
+        #tworzenie default wartości funkcji
+        def moja_funkcja2(pierwszy="Uwaga błąd",drugi=-10001):
+            print(pierwszy,2+drugi)
+
+        moja_funkcja2("Funkcja", -1)
+        moja_funkcja2()
+
+    elif item==2:
+        #inne wywoływanie funkcji 
+        moja_funkcja2(drugi=0,pierwszy="Zamiana miejsca")
+
+    elif item==3:
+        print("\n")
+        spis_ludzi("Ola", "Kuba", "Zuza")
+
+    else:
+        print("\n")
+        spis_ludzi("Ola", "Zuza")
