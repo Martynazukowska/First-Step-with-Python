665e806f0adb84c87e03d8eab20c66ddd09dd9df
Martyna Zukowska <martynazukowska7@interia.pl>
2023-02-14 02:20:14 +0100

    Poczatek, ścieżki i pliki źródłowe

---
+++ Bitwa/classes/enemy.py
@@ -0,0 +1,14 @@
+
+
+class Wrog:
+    def __init__(self,zycie,moc):
+        self.zycie=zycie
+        self.moc=moc
+        self.max_zycie=zycie
+        self.max_moc=moc
+    
+    def getzycie(self):
+        return self.zycie
+    
+    def getmoc(self):
+        return self.moc
---
+++ Bitwa/main.py
@@ -0,0 +1,5 @@
+from classes.enemy import Wrog
+
+enemy=Wrog(200,40)
+
+print(enemy.getmoc())
