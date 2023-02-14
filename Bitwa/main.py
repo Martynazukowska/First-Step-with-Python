29e067aa57d230a16c41b09e5eb417fd330c0f1f
Martyna Zukowska <martynazukowska7@interia.pl>
2023-02-14 22:59:17 +0100

    losuj zaklecie i losuj obrazenie

--- Bitwa/main.py
+++ Bitwa/main.py
@@ -1,5 +1,15 @@
-from classes.enemy import Wrog
+from classes.game import Osoba,bcolors
 
-enemy=Wrog(200,40)
+#słownik do różnych zaklęc
+magic= [{"nazwa": "Fire", "koszt": 10, "obrazenia": 60},
+        {"nazwa": "Thunder", "koszt": 10, "obrazenia": 80},
+        {"nazwa": "Blizzard", "koszt": 20, "obrazenia": 80},
+        {"nazwa": "Water", "koszt": 1, "obrazenia": 10}]
 
-print(enemy.getmoc())
+player1=Osoba(460,65,60,34,magic)
+
+print(player1.losuj_obrazenie())
+print(player1.losuj_magic(0))
+print(player1.losuj_magic(1))
+print(player1.losuj_magic(2))
+print(player1.losuj_magic(3))
