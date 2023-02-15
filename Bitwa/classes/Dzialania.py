3727f819d473b052a9dea43d8bb540c2ab928228
Martyna Zukowska <martynazukowska7@interia.pl>
2023-02-15 16:55:02 +0100

    Fukcja potrzebne do liczby zycia i mocy tak aby bar się nie przesówał

--- Bitwa/classes/Dzialania.py
+++ Bitwa/classes/Dzialania.py
@@ -32,29 +32,53 @@ def bar_moc(osoba):
         i+=1
     return hp_cale
 
+#Funkcja potrzebna aby bar się nie przesówał
+def liczba_zycia(osoba):
+    pom_zycie=str(osoba.get_zycie())
+    pom_zycie_max=str(osoba.get_zycie_max())
+    if len(pom_zycie)-len(pom_zycie_max)<0:
+        pom=len(pom_zycie_max)-len(pom_zycie)
+        while pom > 0:
+            pom_zycie+=" "
+            pom-=1
+    return pom_zycie
+
+
+#Funkcja potrzebna aby bar się nie przesówał
+def liczba_mocy(osoba):
+    pom_mocy=str(osoba.get_moc())
+    pom_moc_max=str(osoba.get_moc_max())
+    if len(pom_mocy)-len(pom_moc_max)<0:
+        pom=len(pom_moc_max)-len(pom_mocy)
+        while pom > 0:
+            pom_mocy+=" "
+            pom-=1
+    return pom_mocy
+
 
 def wypisz(wrog,player1):
-
+    #zycie wroga - bar
     bar=bar_zycie(wrog)
     bar=str(bar)
     bar=re.sub("', ","",bar)
     bar=re.sub("'","",bar)
     print("------------------------------------------------")
     print("      ŻYCIE                                              MOC")
-    print("                  ____________________________")
-    print("Wrog: "+bcolors.FAIL+str(wrog.get_zycie())+bcolors.ENDC+" / "+bcolors.FAIL+str(wrog.get_zycie_max())+bcolors.ENDC+ 
+    print("                   ____________________________")
+    print("Wrog: "+bcolors.FAIL+liczba_zycia(wrog)+bcolors.ENDC+" / "+bcolors.FAIL+str(wrog.get_zycie_max())+bcolors.ENDC+ 
           " |"+bcolors.FAIL+bar+bcolors.ENDC+"|")
 
+    #nasze zycie - bar 
     bar=bar_zycie(player1)
     bar=str(bar)
     bar=re.sub("', ","",bar)
     bar=re.sub("'","",bar)
-
+    #nasza moc - bar
     bar2=bar_moc(player1)
     bar2=str(bar2)
     bar2=re.sub("', ","",bar2)
     bar2=re.sub("'","",bar2)
     print("                  ____________________________                   _____________")
-    print("Ty:   "+bcolors.OKGREEN+str(player1.get_zycie())+bcolors.ENDC+" / "+bcolors.OKGREEN+str(player1.get_zycie_max())+bcolors.ENDC+ 
-          "  |"+bcolors.OKGREEN+bar+bcolors.ENDC+"|         "+bcolors.OKBLUE+str(player1.get_moc())+bcolors.ENDC+" / "+
+    print("Ty:   "+bcolors.OKGREEN+liczba_zycia(player1)+bcolors.ENDC+" / "+bcolors.OKGREEN+str(player1.get_zycie_max())+bcolors.ENDC+ 
+          "  |"+bcolors.OKGREEN+bar+bcolors.ENDC+"|         "+bcolors.OKBLUE+liczba_mocy(player1)+bcolors.ENDC+" / "+
           bcolors.OKBLUE+str(player1.get_moc_max())+bcolors.ENDC+" |"+bcolors.OKBLUE+bar2+bcolors.ENDC+"|")
