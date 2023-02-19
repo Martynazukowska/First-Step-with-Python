ceef6bf2160b7786d8c51b42ed485eee82e16c88
Martyna Zukowska <martynazukowska7@interia.pl>
2023-02-19 23:49:17 +0100

    Wyswietlaj na stronie moj tekst

--- Web_Development/main.py
+++ Web_Development/main.py
@@ -1,1 +1,14 @@
-import web
+import web 
+
+
+#czyli jesli w url bedzie np. http://0.0.0.0:8080/martyna to martyna index
+url=('/(.*)','index')
+
+app=web.application(url,globals())
+
+class index:
+    def GET(self, name):
+        return "<h1> Hej </h1>"+name
+
+if __name__=="__main__":
+    app.run()
