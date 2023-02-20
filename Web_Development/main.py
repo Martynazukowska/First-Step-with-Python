9851bc4e1a57cabe15f0e186489b6eeb9db490a5
Martyna Zukowska <martynazukowska7@interia.pl>
2023-02-20 01:08:12 +0100

    Wyświetl na stronie moją strone html

--- Web_Development/main.py
+++ Web_Development/main.py
@@ -1,14 +1,16 @@
 import web 
 
 
-#czyli jesli w url bedzie np. http://0.0.0.0:8080/martyna to martyna index
-url=('/(.*)','index')
+#czyli jesli w url bedzie np. http://0.0.0.0:8080/martyna/22 to index.name=martyna index.age=22
+url=('/(.*)/(.*)','index')
+
+render=web.template.render("zrodla/")
 
 app=web.application(url,globals())
 
 class index:
-    def GET(self, name):
-        return "<h1> Hej </h1>"+name
+    def GET(self, name,age):
+        return render.main(name,age)
 
 if __name__=="__main__":
     app.run()
